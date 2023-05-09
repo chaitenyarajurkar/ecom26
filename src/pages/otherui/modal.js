18b2685164f5537bd80b40f590b9a43f4c6db457
Chaitenya-rajurkar <rajuchetan547@gmail.com>
2023-05-09 17:57:32 +0530

    modal title

--- src/pages/otherui/modal.js
+++ src/pages/otherui/modal.js
@@ -6,7 +6,7 @@ const Modal = (props) => {
                     <div className="modal-dialog" role="document">
                         <div className="modal-content">
                             <div className="modal-header">
-                                <h5 className="modal-title">Modal title</h5>
+                                <h5 className="modal-title">Modal</h5>
                                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
